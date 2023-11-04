844b472e54cff30640c54c2e4dec32173fb4e0b2
Mastin <martin.pavic97@gmail.com>
2023-11-04 15:59:17 +0100

    feat: add linter, formatter and pre-commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+const config = {
+	semi: true,
+	tabWidth: 2,
+	printWidth: 120,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	trailingComma: 'always',
+	arrowParens: 'always',
+	endOfLine: 'auto',
+  }
+   
+  module.exports = config
